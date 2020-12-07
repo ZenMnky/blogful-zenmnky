a7577901d34a73902c31774e5e80bddc4862b2cd
Justin Hager <coachjustinhager@gmail.com>
2020-12-07 09:51:05 -0800

    add GET /articles endpoint

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+
 const { NODE_ENV } = require('./config')
+const ArticlesService = require('./articles-service')
 
 const app = express();
 
@@ -14,6 +16,15 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+app.get('/articles', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getAllArticles(knexInstance)
+    .then(articles => {
+        res.json(articles)
+    })
+    .catch(next) // pass errors to error handler middleware
+})
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello, boilerplate!')
--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
+const knex = require('knex')
 const app = require('./app')
-const PORT = require('./config')
+const { PORT, DB_URL } = require('./config')
+
+const db = knex({
+    client: 'pg',
+    connection: DB_URL
+})
+
+// attach the Knex instance 'db' to the app as a property called 'db'
+app.set('db', db)
+
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
